5687cc4426010ab11f222d23836302d1d1a62753
Andrei Lisovskii <lisovsky46@gmail.com>
2024-05-28 02:09:10 +0200

    endpoint validation

--- PriceCalculator/Request/CalculationJobItem.cs
+++ PriceCalculator/Request/CalculationJobItem.cs
@@ -1,11 +1,16 @@
-﻿namespace PriceCalculator
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceCalculator
 {
     public class CalculationJobItem
     {
+        [Required]
         public string ItemName { get; set; } = "";
 
+        [Required]
         public decimal ItemPrice { get; set; }
 
+        [Required]
         public bool Exempt { get; set; }
 
         public CalculationJobItem()
--- PriceCalculator/Request/CalculationJobRequest.cs
+++ PriceCalculator/Request/CalculationJobRequest.cs
@@ -1,9 +1,13 @@
-﻿namespace PriceCalculator
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceCalculator
 {
     public class CalculationJobRequest
     {
+        [Required]
         public bool ExtraMargin { get; set; }
 
+        [MinLength(1)]
         public IEnumerable<CalculationJobItem> Items { get; set; } = new CalculationJobItem[0];
 
         public CalculationJobRequest()
